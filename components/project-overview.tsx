b549452a8de9354ffc6331cc496622db8c7f4c22
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-22 17:16:21 +0700

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,12 +1,11 @@
 import NextLink from "next/link";
+
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
       <h1 className="text-3xl font-semibold mb-4">Vercel x Groq Chatbot</h1>
       <p className="text-center">
-        Trợ lý AI <NextLink href="https://tdn-m.com/">@TDNM</Link>{" "}
-      </NextLink>
-        .
+        Trợ lý AI <NextLink href="https://tdn-m.com/">@TDNM</NextLink>{" "}
       </p>
     </div>
   );
