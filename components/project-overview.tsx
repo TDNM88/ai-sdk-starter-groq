121a12f404170f2a63344b11ec1b521c89687ab2
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-22 17:19:25 +0700

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -10,21 +10,3 @@ export const ProjectOverview = () => {
     </div>
   );
 };
-
-const Link = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <NextLink
-      target="_blank"
-      className="text-blue-500 hover:text-blue-600 transition-colors duration-75"
-      href={href}
-    >
-      {children}
-    </NextLink>
-  );
-};
